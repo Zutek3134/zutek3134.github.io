6143712eafd6db95d17c772d31144f230f7a9509
Zutek3134 <51139838+Zutek3134@users.noreply.github.com>
2023-04-11 00:51:21 +0800

    Deployed 436fec4 with MkDocs version: 1.4.2

--- javascripts/cd.js
+++ javascripts/cd.js
@@ -10,23 +10,37 @@ countdowns.forEach(function(countdowns) {
   var x = setInterval(function() {
     var now = new Date().getTime();
     var distance = countDownDate - now;
-    var strIn = lang == "Ch" ? "還有 " : "in ";
-    var strAgo = "";
+    var strIn = lang == "Ch" ? "尚餘" : "in";
+    strIn = "<span class=\"cdtext\">" + strIn + "</span>";
+    var strAgo = "<span class=\"cdtext\">";
     
     if (distance < 0) {
       distance = -distance;
       strIn = "";
-      strAgo = lang == "Ch" ? " 前" : " ago";
+      strAgo += (lang == "Ch" ? "前" : "ago") + "</span>";
     }
 
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    //var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    //var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    var Eng = days + "d " + hours + "h ";
-    var Ch = days + " 日 " + hours + " 時 ";
+    var Eng = "";
+    var Ch = "";
+
+    if (days > 7) {
+      Eng = days + "d";
+      Ch = days + " 日";
+    }
+    else if (days > 0) {
+      Eng = days + "d " + hours + "h";
+      Ch = days + " 日 " + hours + " 時";
+    }
+    else {
+      Eng = (hours > 0 ? hours + "h " : "") + minutes + "m" + (hours < 1 ? " " + seconds + "s" : "");
+      Ch = (hours > 0 ? hours + " 時 " : "") + minutes + " 分" + (hours < 1 ? " " + seconds + " 秒" : "");
+    }
   
-    document.getElementById(id).innerHTML = strIn + (lang == "Ch" ? Ch : Eng) + strAgo;
+    document.getElementById(id).innerHTML = strIn + " " + (lang == "Ch" ? Ch : Eng) + " " + strAgo;
   }, 1000);
 });
