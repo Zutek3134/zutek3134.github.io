1fc960967dcf52e4024664d14ae6ce808638e681
Zutek3134 <51139838+Zutek3134@users.noreply.github.com>
2023-04-25 22:41:57 +0800

    Deployed 2b0da78 with MkDocs version: 1.4.2

--- javascripts/2048/game_manager.js
+++ javascripts/2048/game_manager.js
@@ -40,12 +40,14 @@ GameManager.prototype.setup = function () {
     this.grid        = new Grid(previousState.grid.size,
                                 previousState.grid.cells); // Reload grid
     this.score       = previousState.score;
+    this.step        = previousState.step;
     this.over        = previousState.over;
     this.won         = previousState.won;
     this.keepPlaying = previousState.keepPlaying;
   } else {
     this.grid        = new Grid(this.size);
     this.score       = 0;
+    this.step        = 0;
     this.over        = false;
     this.won         = false;
     this.keepPlaying = false;
@@ -90,6 +92,7 @@ GameManager.prototype.actuate = function () {
 
   this.actuator.actuate(this.grid, {
     score:      this.score,
+    step:       this.step,
     over:       this.over,
     won:        this.won,
     bestScore:  this.storageManager.getBestScore(),
@@ -103,6 +106,7 @@ GameManager.prototype.serialize = function () {
   return {
     grid:        this.grid.serialize(),
     score:       this.score,
+    step:        this.step,
     over:        this.over,
     won:         this.won,
     keepPlaying: this.keepPlaying
