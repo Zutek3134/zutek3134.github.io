a2e53b5fdb06968bab6a75415604a6c50a14f5e8
Zutek3134 <51139838+Zutek3134@users.noreply.github.com>
2023-10-23 00:39:10 +0800

    Deployed 6c13c6c with MkDocs version: 1.4.2

--- zh-TW/javascripts/grids.js
+++ zh-TW/javascripts/grids.js
@@ -4,12 +4,13 @@ function convertMarkdownToHTML(markdownText) {
 
     for (let i = 0; i < lines.length; i++) {
         const line = lines[i].trim();
-        if (line.startsWith('- ![img]')) {
-            const imgSrc = line.match(/\(.*\)/)[0].replace('(', '').replace(')', '');
-            const title = lines[i].split('Title')[1].trim();
-            const description = lines[i + 1].trim();
-
-            htmlOutput += `<div class="grid-item"><img src="${imgSrc}" alt="Image"><h3>${title}</h3><p>${description}</p></div>`;
+        if (line.startsWith('- ')) {
+            htmlOutput += `<div class="grid-item">`;
+            const imgSrc = lines[i].replace('-', '').trim();
+            if (imgSrc.length > 1) htmlOutput += `<img src="` + imgSrc + `" alt="">`;
+            htmlOutput += `<h3>` + lines[i + 1].trim() + `</h3>`;
+            const description = !lines[i + 2].startsWith('- ') ? `<p>` + lines[i + 2].trim() + `</p>` : ``;
+            htmlOutput += description + `</div>`;
 
             // Skip the lines that were used for this grid item
             i += 1;
@@ -20,8 +21,10 @@ function convertMarkdownToHTML(markdownText) {
     return htmlOutput;
 }
 
-const markdownInput = document.getElementById('grids');
-const markdownText = markdownInput.innerHTML;
-const markdownOutput = document.getElementById('grids');
-const htmlOutput = convertMarkdownToHTML(markdownText);
-markdownOutput.innerHTML = htmlOutput;
+const markdownInput = document.getElementById('myGrids');
+if (markdownInput != null) {
+    const markdownText = markdownInput.innerHTML;
+    const markdownOutput = document.getElementById('myGrids');
+    const htmlOutput = convertMarkdownToHTML(markdownText);
+    markdownOutput.innerHTML = htmlOutput;
+}
