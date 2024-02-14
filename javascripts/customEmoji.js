648fd3e3ff8d9ef8ab9839ac3c10493e37b0d67a
Zutek3134 <51139838+Zutek3134@users.noreply.github.com>
2024-02-14 16:45:38 +0800

    Deployed 15ab47c with MkDocs version: 1.5.3

--- javascripts/customEmoji.js
+++ javascripts/customEmoji.js
@@ -3,7 +3,7 @@
     if (emojiContainers) {
       emojiContainers.forEach((emojiContainer) => {
         const text = emojiContainer.innerHTML;
-        const replacedText = text.replace(/:([\w+-]+):/g, '<img src="/Zutek3134/images/emojis/$1.png" alt="$1" title=":$1:" class="emoji">');
+        const replacedText = text.replace(/:([\w+-]+):/g, '<img src="/images/emojis/$1.png" alt="$1" title=":$1:" class="emoji">');
         emojiContainer.innerHTML = replacedText;
       });
     }
